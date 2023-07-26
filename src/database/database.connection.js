a55896fad85b43eee148800815120b4c4a1b14ea
Brendo <brendow_prime@hotmail.com>
2023-07-26 00:19:50 -0400

    porta atualizada => 5000

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -9,6 +9,13 @@ const configDatabase = {
     connectionString: process.env.DATABASE_URL
 }
 
+/* try {
+    await configDatabase.connect();
+    console.log("Postgres CONECTADO!")
+} catch (err) {
+    console.log(err.message)
+} */
+
 if (process.env.MODE === "production") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase)
