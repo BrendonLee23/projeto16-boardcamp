7437958ab642fcb2fdd3385712222d5d49e3b848
Brendo <brendow_prime@hotmail.com>
2023-07-31 00:15:26 -0400

    atualização da rota customers

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
 /* import receitasRouter from "./receitas.routes.js" */
 import gamesRouter from "./games.routes.js";
+import customersRouter from "./customers.routes.js";
 
 const router = Router()
 /* router.use(receitasRouter) */
 router.use(gamesRouter)
+router.use(customersRouter)
 
-export default router
+export default router;
