79a05aff4343a0a9b5df2ddc706d8ab0926b8c1a
Brendo <brendow_prime@hotmail.com>
2023-07-31 10:20:04 -0400

    correção de postGaes

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -12,10 +12,12 @@ export async function postGames(req, res) {
     const{name, image, stockTotal, pricePerDay} = req.body;
 
     try {
-        const game = await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)`,  [name, image, stockTotal, pricePerDay]);
-        if(game.rows.length > 0){
-            return res.status(409).send("O jogo já existe");
+        const gameJaExiste = await db.query(`SELECT FROM games WHERE name = $1`, [name])
+        if(gameJaExiste.rows.length>0){
+            return res.status(409).send("Erro ao inserir jogo. O jogo já existe")
         }
+        const game = await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)`,  [name, image, stockTotal, pricePerDay]);
+
         res.sendStatus(201)
     } catch (err) {
         res.status(500).send(err.message)
