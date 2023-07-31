86a136434bb9b0bd25b61026b472dbdf446e8ba3
Brendo <brendow_prime@hotmail.com>
2023-07-31 13:27:16 -0400

    get rentals aplicado

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -7,7 +7,17 @@ export async function getRentals(req, res) {
             games.name as game FROM rentals JOIN customers ON 
             rentals."customerId" = customers.id JOIN games ON rentals."gameId"=games.id;
         `);
-        
+        rentals.rows.forEach(rental => {
+            const rentDate = new Date(rental.rentDate);
+            rental.rentDate = `${rentDate.getFullYear()}-${String(rentDate.getMonth() + 1).padStart(2, '0')}-${String(rentDate.getDate()).padStart(2, '0')}`
+            rental.customer = { id: rental.customerId, name: rental.customer };
+            rental.game = { id: rental.gameId, name: rental.game };
+            if (rental.returnDate) {
+                const returnDate = new Date(rental.returnDate);
+                rental.returnDate = `${returnDate.getFullYear()}-${String(returnDate.getMonth() + 1).padStart(2, '0')}-${String(returnDate.getDate()).padStart(2, '0')}`
+            }
+        })
+        res.send(rentals.rows);
     } catch (err) {
         res.status(500).send(err.message)
     }
@@ -18,7 +28,7 @@ export async function postRentals(req, res) {
     try {
         const game = await db.query(`SELECT * FROM games WHERE id=$1`, [gameId]);
         const rentDate = new Date();
-        const originalPrice = daysRented*game.rows[0].pricePerDay;
+        const originalPrice = daysRented * game.rows[0].pricePerDay;
         await db.query(`
             INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
             VALUES ($1,$2,$3,$4, null, $5, null);
@@ -28,13 +38,7 @@ export async function postRentals(req, res) {
         res.status(500).send(error.message);
     }
 }
-export async function finalizeRentals(req, res) {
-    try {
 
-    } catch (err) {
-        res.status(500).send(err.message)
-    }
-}
 export async function deleteRentals(req, res) {
     try {
 
