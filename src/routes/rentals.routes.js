dcb58779292ed82787da9c54280a91b94fee688c
Brendo <brendow_prime@hotmail.com>
2023-07-31 11:27:13 -0400

    correção de rotas rentals

--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { validateRentals } from "../middlewares/validadeRentals.middleware";
-import { rentalsSchema } from "../schemas/rentals.chema";
-import { deleteRentals, finalizeRentals, getRentals, postRentals } from "../controllers/rentals.controller";
+import { validateRentals } from "../middlewares/validadeRentals.middleware.js";
+import { rentalsSchema } from "../schemas/rentals.schema.js";
+import { deleteRentals, finalizeRentals, getRentals, postRentals } from "../controllers/rentals.controller.js";
 
 
 const rentalsRouter = Router();
