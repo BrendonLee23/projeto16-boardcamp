a0eadd561eb353e0443b3c5d2fb0f810ec1ce417
Brendo <brendow_prime@hotmail.com>
2023-07-31 10:57:21 -0400

    correção post Customers

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -24,14 +24,11 @@ export async function postCustomers(req, res) {
     const {name, phone, cpf, birthday} = req.body;
 
     try {
-        const cpfJaExiste = await db.query(`SELECT FROM games WHERE cpf = $3`, [cpf])
+        const cpfJaExiste = await db.query(`SELECT FROM games WHERE cpf = $1`, [cpf])
         if(cpfJaExiste.rows.length>0){
-            return res.status(409).send("Erro ao Cadastrar. O CPF já existe")
+            return res.status(409).send("Erro ao Cadastrar. CPF já cadastado")
         }
         const customer = await db.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)`,  [name, phone, cpf, birthday]);
-        if(!customer){
-            return res.sendStatus(400);
-        }
         res.sendStatus(201)
     } catch (err) {
         res.status(500).send(err.message)
@@ -43,14 +40,10 @@ export async function editCustomers(req, res) {
     const {name, phone, cpf, birthday} = req.body;
 
     try{
-        const customers = await (await db.query(`SELECT * FROM customers`)).rows;
-        const verificarCpf = customers.find((c) => c.cpf == cpf);
-        if(verificarCpf){
-            if(verificarCpf.id != id){
-                return res.status(409).send("Cpf já cadastrado em outro cliente");
-            }
+        const cpfJaExiste = await db.query(`SELECT FROM games WHERE cpf = $3`, [cpf])
+        if(cpfJaExiste.rows.length>0){
+            return res.status(409).send("Erro ao Cadastrar. CPF já cadastado")
         }
-
         await db.query(`UPDATE customers SET name='${name}', phone='${phone}', cpf='${cpf}', birthday='${birthday}' WHERE id=$1;`, [id]);
         res.sendStatus(200);
     }catch (err) {
