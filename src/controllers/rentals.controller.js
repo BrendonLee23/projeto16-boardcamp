5da6f944ca4e045369a7d52ab31452fe60a3a847
Brendo <brendow_prime@hotmail.com>
2023-07-31 13:08:01 -0400

    Post Rentals Aplicado

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -2,16 +2,25 @@ import { db } from "../database/database.connection.js"
 
 export async function getRentals(req, res) {
     try {
-        db
+        
     } catch (err) {
         res.status(500).send(err.message)
     }
 }
 export async function postRentals(req, res) {
+    const { customerId, gameId, daysRented } = req.body;
+    if (daysRented <= 0) return res.status(400).send('daysRented deve ser maior que zero!');
     try {
-
-    } catch (err) {
-        res.status(500).send(err.message)
+        const game = await db.query(`SELECT * FROM games WHERE id=$1`, [gameId]);
+        const rentDate = new Date();
+        const originalPrice = daysRented*game.rows[0].pricePerDay;
+        await db.query(`
+            INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+            VALUES ($1,$2,$3,$4, null, $5, null);
+        `, [customerId, gameId, rentDate, daysRented, originalPrice]);
+        res.sendStatus(201);
+    } catch (error) {
+        res.status(500).send(error.message);
     }
 }
 export async function finalizeRentals(req, res) {
